a7f32e284192dd5a65acd2586437c1d948dcc4e3
zrobera <77199653+zrobera@users.noreply.github.com>
2023-04-11 16:30:19 +0300

    Create min_max_sum.py

---
+++ min_max_sum.py
@@ -0,0 +1,7 @@
+class Solution:
+    def minPairSum(self, nums: List[int]) -> int:
+        pair_sums = []
+        nums.sort()
+        for i in range(len(nums)//2):
+            pair_sums.append(nums[i]+ nums[-(i+1)])
+        return max(pair_sums)
