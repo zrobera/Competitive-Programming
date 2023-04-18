6b4cb02d4273e57a370a2ab6d41a54625d1c0f6a
Robel Tesfaye <robeltesfaye428@gmail.com>
2023-04-18 23:48:48 +0300

    Update bubble_sort.py

--- bubble_sort.py
+++ bubble_sort.py
@@ -7,5 +7,5 @@ def countSwaps(a):
                 swaps +=1
                                 
     print ("Array is sorted in "+str(swaps)+" swaps.")
-    print ("First Element: "+ str(a[0]))
+    print ("First Ellement: "+ str(a[0]))
     print ("Last Element: "+str(a[len(a)-1]))
