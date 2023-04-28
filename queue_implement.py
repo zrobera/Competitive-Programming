5d570d804b267302d79ac2f01592077d85f296d2
Robel Tesfaye <robeltesfaye428@gmail.com>
2023-04-28 22:15:01 +0300

    Update queue_implement.py

--- queue_implement.py
+++ queue_implement.py
@@ -22,6 +22,6 @@ class MyQueue:
 # Your MyQueue object will be instantiated and called as such:
 # obj = MyQueue()
 # obj.push(x)
-# param_2 = obj.pop()
+# para_2 = obj.pop()
 # param_3 = obj.peek()
 # param_4 = obj.empty()
