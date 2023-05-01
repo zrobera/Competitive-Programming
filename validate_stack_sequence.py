08cf5e10a9e39a75fa0ab3ee42b4270c8ddc0f97
Robel Tesfaye <robeltesfaye428@gmail.com>
2023-05-01 23:19:23 +0300

    Create validate_stack_sequence.py

---
+++ validate_stack_sequence.py
@@ -0,0 +1,16 @@
+class Solution:
+    def validateStackSequences(self, pushed: List[int], popped: List[int]) -> bool:
+        i = j = 0
+        stack = []
+        while True:
+            while stack and popped[j] == stack[-1]:
+                stack.pop()
+                j += 1
+            if i >= len(pushed):
+                break
+            stack.append(pushed[i])
+            i += 1
+        if stack:
+            return False
+        return True
+        
