fe144cc264180b201c76ecc7c6923bb70330e5ce
Robel Tesfaye <robeltesfaye428@gmail.com>
2023-05-08 00:42:34 +0300

    Create basic_calc.py

---
+++ basic_calc.py
@@ -0,0 +1,20 @@
+class Solution:
+    def calculate(self, s: str) -> int:
+        stack = []
+        num = 0
+        op = '+'
+        for i, c in enumerate(s):
+            if c.isdigit():
+                num = num * 10 + int(c)
+            if (not c.isdigit() and c != ' ') or i == len(s) - 1:
+                if op == '+':
+                    stack.append(num)
+                elif op == '-':
+                    stack.append(-num)
+                elif op == '*':
+                    stack[-1] *= num
+                elif op == '/':
+                    stack[-1] = int(stack[-1] / num)
+                num = 0
+                op = c
+        return sum(stack)
