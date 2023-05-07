c88353721c7a3a9ea3e4915eee1d6b554db9f517
Robel Tesfaye <robeltesfaye428@gmail.com>
2023-05-08 00:38:51 +0300

    Create array_unique.py

---
+++ array_unique.py
@@ -0,0 +1,10 @@
+class Solution:
+    def minIncrementForUnique(self, nums: List[int]) -> int:
+        nums.sort()
+        moves = 0
+        for i in range(1, len(nums)):
+            if nums[i] <= nums[i-1]:
+                moves += nums[i-1] - nums[i] + 1
+                nums[i] = nums[i-1] + 1
+        return moves
+            
