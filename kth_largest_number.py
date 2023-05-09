3945d45a1401bcd4922e109055acda649cc2099b
Robel Tesfaye <robeltesfaye428@gmail.com>
2023-05-09 23:04:03 +0300

    Update kth_largest_number.py

--- kth_largest_number.py
+++ kth_largest_number.py
@@ -1,5 +1,5 @@
 class Solution:
-    def kthLargestNumber(self, nums: List[str], k: int) -> str:
+    def kthLargestNumber(self,nums: List[str], k: int) -> str:
         numbers = list(map(int, nums ))
         numbers.sort(reverse = True)
         return str(numbers[k-1])
