f78be72b6b70e1a2c42cfca79245537de5d557fe
Robel Tesfaye <robeltesfaye428@gmail.com>
2023-06-03 22:38:53 +0300

    Update 344-reverse-string.py

--- 344-reverse-string/344-reverse-string.py
+++ 344-reverse-string/344-reverse-string.py
@@ -3,11 +3,11 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        def reverse_helper(left: int, right: int) -> None:
+        def reverser(left: int, right: int) -> None:
             if left >= right:
                 return
             
             s[left], s[right] = s[right], s[left]
-            reverse_helper(left + 1, right - 1)
+            reverser(left + 1, right - 1)
         
-        reverse_helper(0, len(s) - 1)
+        reverser(0, len(s) - 1)
