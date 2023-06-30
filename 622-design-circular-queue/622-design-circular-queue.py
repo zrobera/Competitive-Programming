f9d0050696b61c60d9ccb91bae426a6845e6921d
Robel Tesfaye <robeltesfaye428@gmail.com>
2023-06-30 21:27:54 +0300

    Time: 76 ms (83.52%), Space: 16.8 MB (90.43%) - LeetHub

--- 622-design-circular-queue/622-design-circular-queue.py
+++ 622-design-circular-queue/622-design-circular-queue.py
@@ -1,36 +1,52 @@
 class MyCircularQueue:
 
     def __init__(self, k: int):
-        self.q = []
-        self.k = k
+        self.capacity = k
+        self.size = 0
+        self.data = [0 for _ in range(k)]
+        self.left, self.right = 0, -1
+
+
     def enQueue(self, value: int) -> bool:
-        if not self.isFull():
-            self.q.append(value)
-            return True
+        if self.isFull():   return False
         
+        self.right += 1
+        self.right %= self.capacity
+        self.data[self.right] = value
+        self.size += 1
+
+        return True
         
+
     def deQueue(self) -> bool:
-        if not self.isEmpty():
-            self.q.pop(0)
-            return True
+        if self.isEmpty():  return False
+
+        self.left += 1
+        self.left %= self.capacity
+        self.size -= 1
+
+        return True
         
-        
+
     def Front(self) -> int:
-        if self.isEmpty(): return -1
-        return self.q[0]
+        if self.isEmpty():  return -1
+
+        return self.data[self.left]
         
 
     def Rear(self) -> int:
-        if self.isEmpty(): return -1
+        if self.isEmpty():  return -1
+
+        return self.data[self.right]
         
-        return self.q[-1]
 
     def isEmpty(self) -> bool:
-        return len(self.q) == 0
+        return self.size == 0
         
 
     def isFull(self) -> bool:
-        return len(self.q) == self.k
+        return self.size == self.capacity
+        
 
 
 # Your MyCircularQueue object will be instantiated and called as such:
