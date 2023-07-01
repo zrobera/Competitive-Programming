d93fd2d1160134905690bea62400893d33b9a3e5
Robel Tesfaye <robeltesfaye428@gmail.com>
2023-07-01 23:17:16 +0300

    Update car_fleet.py

--- car_fleet.py
+++ car_fleet.py
@@ -1,12 +1,9 @@
 class Solution:
-    def carFleet(self, target: int, position: list[int], speed: List[int]) -> int:
-        cars = sorted(zip(position,speed), reverse = True)
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        cars = sorted([(position[i], speed[i]) for i in range(len(position))], reverse=True)
         fleets = []
         for pos,spd in cars:
             time_to_dist = (target - pos) / spd
-            if not fleets or time_to_dist > fleets[-1][1]:
-                fleets.append((pos, time_to_dist))
-            else:
-                continue
-        
+            if not fleets or time_to_dist > fleets[-1]:
+                fleets.append(time_to_dist)     
         return len(fleets)            
