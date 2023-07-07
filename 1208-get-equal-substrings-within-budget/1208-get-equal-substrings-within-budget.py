7b28896c824f0318c377e5fe704e8370d66b3908
Robel Tesfaye <robeltesfaye428@gmail.com>
2023-07-08 01:03:03 +0300

    Time: 107 ms (56.84%), Space: 17.2 MB (36.79%) - LeetHub

---
+++ 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.py
@@ -0,0 +1,12 @@
+class Solution:
+    def equalSubstring(self, s: str, t: str, maxCost: int) -> int:
+        cost = 0
+        ans = 0
+        j = 0
+        for i in range(len(s)):
+            cost += abs(ord(s[i]) - ord(t[i]))
+            while cost > maxCost:
+                cost -= abs(ord(s[j]) - ord(t[j]))
+                j += 1
+            ans = max(ans, i - j + 1)
+        return ans
