c2e5fdf72df00e5dfda7b0f41ee8cc0ddedb62a0
Robel Tesfaye <robeltesfaye428@gmail.com>
2023-08-25 18:08:06 +0300

    Time: 308 ms (65.73%), Space: 18.8 MB (88.75%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,26 +1,23 @@
 class Solution:
-    def foundIsland(self, row: int, col: int,grid: List[List[str]], visited:Optional) -> bool:
+    def foundIsland(self, row: int, col: int,grid: List[List[str]]) -> bool:
         if (row < 0 or row >= len(grid)) or (col < 0 or col >= len(grid[0])):
             return False
         if grid[row][col] == "0":
             return False
-        if (row,col) in visited:
-            return False
-        visited.add((row,col))
+        grid[row][col] = "0"
 
         for r,c in [(1,0), (0,1), (-1,0), (0,-1)]:
             new_r, new_c = row+r, col+c
-            self.foundIsland(new_r, new_c, grid, visited)
+            self.foundIsland(new_r, new_c, grid)
         return True
 
 
 
     def numIslands(self, grid: List[List[str]]) -> int:
         r,c = len(grid), len(grid[0])
-        visited = set()
         count = 0
         for row in range(r):
             for col in range(c):
-                if self.foundIsland(row,col,grid, visited):
+                if self.foundIsland(row,col,grid):
                     count += 1
         return count
