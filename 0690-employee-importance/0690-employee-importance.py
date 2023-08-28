925d0c8184dd08f5d6063bfa962caa63f04207ef
Robel Tesfaye <robeltesfaye428@gmail.com>
2023-08-28 23:11:33 +0300

    Time: 141 ms (60.84%), Space: 17.8 MB (48.09%) - LeetHub

---
+++ 0690-employee-importance/0690-employee-importance.py
@@ -0,0 +1,19 @@
+"""
+# Definition for Employee.
+class Employee:
+    def __init__(self, id: int, importance: int, subordinates: List[int]):
+        self.id = id
+        self.importance = importance
+        self.subordinates = subordinates
+"""
+
+class Solution:
+    def getImportance(self, employees: List['Employee'], id: int) -> int:
+        employee_dict = {employee.id: employee for employee in employees}
+
+        def getTotal(id):
+            employee = employee_dict[id]
+            return employee.importance + sum([getTotal(sub_id) for sub_id in employee.subordinates])
+            
+        return getTotal(id)
+        
