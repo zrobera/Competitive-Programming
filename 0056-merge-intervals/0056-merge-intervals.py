f28c98198e15fb3a7ebd1aa3fdfc332547cb9e5f
Robel Tesfaye <robeltesfaye428@gmail.com>
2023-10-07 16:06:18 +0300

    Time: 127 ms (93.25%), Space: 21.1 MB (72.14%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -0,0 +1,10 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort(key=lambda x: x[0])
+        merged = []
+        for interval in intervals:
+            if merged and interval[0] <= merged[-1][1]:
+                merged[-1][1] = max(merged[-1][1], interval[1])
+            else:
+                merged.append(interval)
+        return merged
