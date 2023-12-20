26f2aede9800ba54088d958c5c7bc2f37707b56c
Robel Tesfaye <robeltesfaye428@gmail.com>
2023-12-20 17:17:07 +0300

    Added solution for Reverse String

---
+++ G54/reverse-string.py
@@ -0,0 +1,13 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        def reverse_helper(left: int, right: int) -> None:
+            if left >= right:
+                return
+            
+            s[left], s[right] = s[right], s[left]
+            reverse_helper(left + 1, right - 1)
+        
+        reverse_helper(0, len(s) - 1)
