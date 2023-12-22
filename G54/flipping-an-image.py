3e6135cd7d758ef92a643f717aa9bfbbfaf9c18f
Robel Tesfaye <robeltesfaye428@gmail.com>
2023-12-22 17:36:36 +0300

    Added solution for Flipping an Image

---
+++ G54/flipping-an-image.py
@@ -0,0 +1,15 @@
+class Solution:
+    def flipAndInvertImage(self, image: List[List[int]]) -> List[List[int]]:
+        for row in image:
+            row.reverse()
+            for i in range(len(row)):
+                if row[i] == 0:
+                    row[i] = 1
+                else:
+                    row[i] = 0
+        return image
+
+
+
+
+        
