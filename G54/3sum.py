ad70e3c46460652fc6499c760863c27dc89e7b0e
Robel Tesfaye <robeltesfaye428@gmail.com>
2024-01-08 22:40:40 +0300

    Added solution for 3Sum

--- G54/3sum.py
+++ G54/3sum.py
@@ -1,31 +1,22 @@
 class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
-        res = set()
-        n, p, z = [], [], []
-        for num in nums:
-            if num > 0:
-                p.append(num)
-            elif num < 0: 
-                n.append(num)
-            else:
-                z.append(num)
-        N, P = set(n), set(p)
-        if z:
-            for num in P:
-                if -1*num in N:
-                    res.add((-1*num, 0, num))
-        if len(z) >= 3:
-            res.add((0,0,0))
-
-        for i in range(len(n)):
-            for j in range(i+1,len(n)):
-                target = -1*(n[i]+n[j])
-                if target in P:
-                    res.add(tuple(sorted([n[i],n[j],target])))
-        for i in range(len(p)):
-            for j in range(i+1,len(p)):
-                target = -1*(p[i]+p[j])
-                if target in N:
-                    res.add(tuple(sorted([p[i],p[j],target])))
-
-        return res
+    def threeSum(self, nums: List[int]) -> List[List[int]]: 
+        nums.sort() 
+        ans = []
+        for i in range(len(nums)):
+            if i>0 and nums[i-1]==nums[i]:
+                continue 
+	
+            j=i+1 
+            k=len(nums)-1
+            while j<k: 
+                s=nums[i]+nums[j]+nums[k] 
+                if s>0:
+                    k-=1
+                elif s<0: 
+                    j+=1 
+                else:
+                    ans.append([nums[i],nums[j],nums[k]])
+                    j+=1 
+                    while nums[j-1]==nums[j] and j<k:
+                        j+=1   
+        return ans
