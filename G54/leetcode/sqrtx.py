667ea1bc1554d9ac30473fb7bbe9c7a466d99a90
Robel Tesfaye <robeltesfaye428@gmail.com>
2024-03-14 17:54:39 +0300

    Add solution for Sqrt(x)

---
+++ G54/leetcode/sqrtx.py
@@ -0,0 +1,13 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        low, high = 0, x//2+1
+        while low <= high:
+            mid = low + (high-low)//2
+            if mid**2 == x:
+                return mid
+            elif mid**2 < x:
+                low = mid+1
+            else:
+                high = mid -1
+        return high
+        
